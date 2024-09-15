188ebbbb546d13dc5674c321d0333ddf37a7f27e
wnd01jun <changjun157@naver.com>
2024-09-15 19:21:45 +0900

    [feat] UserRepository 추가

---
+++ src/main/java/yourssu/assignment/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package yourssu.assignment.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import yourssu.assignment.domain.user.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /*
+        email 중복 검증을 위해 사용
+     */
+    Optional<User> findByEmail(String email);
+}
