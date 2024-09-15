034e0b6cea48db74d8d2149396990ef96760046e
wnd01jun <changjun157@naver.com>
2024-09-15 15:46:10 +0900

    [feat] BaseEntity 추가

---
+++ src/main/java/yourssu/assignment/domain/basic/BaseEntity.java
@@ -0,0 +1,28 @@
+package yourssu.assignment.domain.basic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
