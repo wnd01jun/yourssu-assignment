6fa9668fd04dac5cd72be99bb29872eef7fea555
wnd01jun <changjun157@naver.com>
2024-09-16 21:49:14 +0900

    [feat] UserRequestDTO 각 DTO에 @builder 추가

--- src/main/java/yourssu/assignment/domain/user/dto/UserRequestDTO.java
+++ src/main/java/yourssu/assignment/domain/user/dto/UserRequestDTO.java
@@ -2,6 +2,7 @@ package yourssu.assignment.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Getter;
 import yourssu.assignment.domain.user.validation.ExistEmail;
 import yourssu.assignment.domain.user.validation.NotExistEmail;
@@ -9,6 +10,7 @@ import yourssu.assignment.domain.user.validation.NotExistEmail;
 public class UserRequestDTO {
 
     @Getter
+    @Builder
     public static class UserRegisterRequest {
         @NotBlank
         @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
@@ -21,6 +23,7 @@ public class UserRequestDTO {
     }
 
     @Getter
+    @Builder
     public static class UserWithdrawRequest {
         @ExistEmail
         private String email;
