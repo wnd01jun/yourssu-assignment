51f8b72c8a265c38a098c29766d3233c85d04bf3
wnd01jun <changjun157@naver.com>
2024-09-19 23:19:47 +0900

    [feat] Controller Class에 validated 추가

--- src/main/java/yourssu/assignment/domain/comment/controller/CommentController.java
+++ src/main/java/yourssu/assignment/domain/comment/controller/CommentController.java
@@ -2,39 +2,41 @@ package yourssu.assignment.domain.comment.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import yourssu.assignment.domain.comment.dto.CommentRequestDTO;
 import yourssu.assignment.domain.comment.dto.CommentResponseDTO;
 import yourssu.assignment.domain.comment.service.CommentService;
 import yourssu.assignment.domain.comment.validation.ExistComment;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/Comments")
+    @PostMapping("/comments")
     public CommentResponseDTO.CommentPostResponse postComment(
             @RequestBody @Valid CommentRequestDTO.CommentPostRequest dto
     ) {
         return commentService.postComment(dto);
     }
 
-    @PutMapping("/Comments/{CommentId}")
+    @PutMapping("/comments/{commentId}")
     public CommentResponseDTO.CommentModifyResponse modifyComment(
             @RequestBody @Valid CommentRequestDTO.CommentModifyRequest dto,
-            @PathVariable @ExistComment @Valid Long CommentId
+            @PathVariable(name = "commentId") @ExistComment @Valid Long commentId
     ) {
-        return commentService.modifyComment(dto, CommentId);
+        return commentService.modifyComment(dto, commentId);
     }
 
-    @DeleteMapping("/Comments/{CommentId}")
+    @DeleteMapping("/comments/{commentId}")
     public String deleteComment(
             CommentRequestDTO.CommentWithdrawRequest dto,
-            @PathVariable @ExistComment @Valid Long CommentId
+            @PathVariable(name = "commentId") @ExistComment @Valid Long commentId
     ) {
-        commentService.withdrawComment(dto, CommentId);
+        commentService.withdrawComment(dto, commentId);
         return "ok";
     }
 }
