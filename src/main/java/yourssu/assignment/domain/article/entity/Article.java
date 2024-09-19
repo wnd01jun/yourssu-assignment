46a17b37d46d7cad8369d40df42984c1cdf9d5d5
wnd01jun <changjun157@naver.com>
2024-09-19 09:57:39 +0900

    [feat] 각 Entity @EqualsAndHashcode 추가

--- src/main/java/yourssu/assignment/domain/article/entity/Article.java
+++ src/main/java/yourssu/assignment/domain/article/entity/Article.java
@@ -1,10 +1,7 @@
 package yourssu.assignment.domain.article.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import yourssu.assignment.domain.article.dto.ArticleRequestDTO;
 import yourssu.assignment.domain.basic.BaseEntity;
@@ -18,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @SuperBuilder
+@EqualsAndHashCode(callSuper=false)
 public class Article extends BaseEntity {
 
     @Id
